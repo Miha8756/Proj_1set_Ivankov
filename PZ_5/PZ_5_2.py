d3157ed2b03c82f5912af66b901547751d03929c
Mikhail_Ivankov <57402171+Miha8756@users.noreply.github.com>
2021-11-20 14:37:34 +0300

    Add files via upload

---
+++ PZ_5/PZ_5_2.py
@@ -0,0 +1,21 @@
+def AddRightDigit(D, K):
+    K = a
+    D = D1
+
+
+
+
+
+
+
+
+
+
+
+
+a = int(input('Введите K: '))
+b1 = int(input('Введите D1: '))
+print()
+b2 = int(input('Введите D2: '))
+print()
+
