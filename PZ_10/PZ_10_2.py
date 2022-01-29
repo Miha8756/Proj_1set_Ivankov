92ed547c7f1bc38eb861cdba723ec4c320ba9e35
Mikhail_Ivankov <Miha8756@yandex.ru>
2022-01-29 23:07:19 +0300

    small commit

--- PZ_10/PZ_10_2.py
+++ PZ_10/PZ_10_2.py
@@ -1,3 +1,6 @@
+#  Из предложенного текстового файла (text18-7.txt) вывести на экран его содержимое, количество букв
+#  в нижнем регистре. Сформировать новый файл, в который поместить текст в стихотворной форме,
+#  предварительно поставив последнюю строку между второй и третьей.
 print(open('text18-7.txt').read() + str(sum(map(str.islower, open('text18-7.txt').read()))))
 print('\n'.join(open('text18-7.txt').read().splitlines()[:2]), '\n' + open('text18-7.txt').read().splitlines()[-1],
       '\n' + '\n'.join(open('text18-7.txt').read().splitlines()[2:6]), file=open('new-text18-7.txt', 'w'))
