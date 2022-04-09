5ee9c3875fbe2d44b1ecb5fbf3ca51dca1b5776c
Mikhail_Ivankov <Miha8756@yandex.ru>
2022-04-09 22:42:02 +0300

    small commit

--- PZ_15/PZ_15_2.py
+++ PZ_15/PZ_15_2.py
@@ -1,13 +1,12 @@
 #  В матрице элементы последнего столбца заменить на -1
 from random import randint
 
+#  Заносим в переменные параметры матрицы
 m, n, y, z = [int(input(i)) for i in ("Количество строк = ", "Количество столбцов = ", "От = ", "До = ")]
+#  заполняем матрицу случайными числами
 matrix = [[randint(y, z) for _ in range(n)] for j in range(m)]
-print('Исходная матрица')
-for i in matrix:
-    print(*i)
+print('Исходная матрица:', '\n', [i for i in matrix])
+#  цикл заменения последних элементов в строке на -1
 for i in range(m):
-    matrix[i][n-1] = -1
-print('Полученная матрица')
-for i in matrix:
-    print(*i)
+    matrix[i][n - 1] = -1
+print('Полученная матрица:', '\n', [i for i in matrix])
